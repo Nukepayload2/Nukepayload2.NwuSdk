fa49d0d8a89ef76c7c9346755345d62cf5c56beb
Nukepayload2 <1939357182@qq.com>
2016-04-14 21:22:11 +0800

    代码移走了
    移动到了共享项目

--- Common/LoginException.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class LoginException
-    Inherits Exception
-    Sub New(Reason$)
-        MyBase.New("登录失败。" & Reason)
-    End Sub
-End Class
--- DoorSite/SchoolNewsGroup.vb
+++
@@ -1,6 +0,0 @@
-﻿Namespace DoorSite
-    Public Class SchoolNewsGroup
-        Public ReadOnly Property Header$
-        Public ReadOnly Property Items As IEnumerable(Of TextLinkPair)
-    End Class
-End Namespace
